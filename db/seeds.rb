17b7e7c7ba59194319dbff988fe1d887254dd4d3
kaityhallman <kaitlinhallman@gmail.com>
2016-02-15 23:01:48 -0500

    added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 List.create!([
-  {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1, image: "tires.jpg", remote_image_url: nil},
-  {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5, image: nil, remote_image_url: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg"},
+  {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1, image: "http://www.tirerack.com/images/facebook/tires/tires_ug_fb.jpg", remote_image_url: nil},
+  {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5, image: "http://gloimg.rosewholesale.com/ROSE/2015/201501/source-img/1420655252103-P-2344128.jpg?01", remote_image_url: nil},
   {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3, image: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg", remote_image_url: nil},
-  {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2, image: "malm-high-bed-frame-storage-boxes__0173784_PE328440_S4.JPG", remote_image_url: nil},
-  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3, image: "41gBSNXd2VL.jpg", remote_image_url: nil},
-  {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5, image: "dual_faucet_kegerators031813114530.jpg", remote_image_url: nil},
-  {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4, image: "lgwebostvsmart1.jpg", remote_image_url: nil}
+  {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2, image: "http://www.ikea.com/PIAimages/0217641_PE375160_S3.JPG", remote_image_url: nil},
+  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3, image: "http://ecx.images-amazon.com/images/I/41gBSNXd2VL._SY300_.jpg", remote_image_url: nil},
+  {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5, image: "http://www.homewetbar.com/images/prod/kegerator-beer-fridge-tap-open49264.jpg", remote_image_url: nil},
+  {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4, image: "http://torontotvrepair.com/uploads/2/7/8/5/2785621/4388679_orig.png", remote_image_url: nil}
 ])
