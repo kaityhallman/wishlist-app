c283b9f579a5ebe939730cc30e1a2d8eb6c4f15b
kaityhallman <kaitlinhallman@gmail.com>
2016-02-15 21:35:47 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-lists = List.create ( [ { name: 'French Press', category: 'Kitchen', price: 39.99, size: 'Medium', priority: 2 } ] )
+List.create!([
+  {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1, image: "tires.jpg", remote_image_url: nil},
+  {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5, image: "adfc063f04403a6f0399f63857bf4875.jpg", remote_image_url: nil},
+  {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3, image: "220-volt-kitchenaid-artisan-stand-mixer-empire-red.jpg", remote_image_url: nil},
+  {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2, image: "malm-high-bed-frame-storage-boxes__0173784_PE328440_S4.JPG", remote_image_url: nil},
+  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3, image: "41gBSNXd2VL.jpg", remote_image_url: nil},
+  {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5, image: "dual_faucet_kegerators031813114530.jpg", remote_image_url: nil},
+  {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4, image: "lgwebostvsmart1.jpg", remote_image_url: nil}
+])
