9ee31b22745528ef42fb17141a40a358ad2d15e6
kaityhallman <kaitlinhallman@gmail.com>
2016-02-16 00:46:44 -0500

    image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 List.create!([
-  {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1,  remote_image_url: "http://www.tirerack.com/images/facebook/tires/tires_ug_fb.jpg"},
-  {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5, remote_image_url: "http://gloimg.rosewholesale.com/ROSE/2015/201501/source-img/1420655252103-P-2344128.jpg?01"},
-  {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3, remote_image_url: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg"},
-  {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2, remote_image_url: "http://www.ikea.com/PIAimages/0217641_PE375160_S3.JPG"},
-  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3, remote_image_url: "http://ecx.images-amazon.com/images/I/41gBSNXd2VL._SY300_.jpg"},
-  {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5, remote_image_url: "http://www.homewetbar.com/images/prod/kegerator-beer-fridge-tap-open49264.jpg"},
-  {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4, remote_image_url: "http://torontotvrepair.com/uploads/2/7/8/5/2785621/4388679_orig.png"}
+  {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1},
+  {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5},
+  {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3},
+  {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2},
+  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3},
+  {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5},
+  {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4}
 ])
