0ad1c846d14e919b90d5894151289996ed315ee9
kaityhallman <kaitlinhallman@gmail.com>
2016-02-15 22:44:39 -0500

    changed config file for eb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 List.create!([
   {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1, image: "tires.jpg", remote_image_url: nil},
   {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5, image: nil, remote_image_url: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg"},
-  {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3, image: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg"), remote_image_url: nil},
+  {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3, image: "http://ecx.images-amazon.com/images/I/81UknzEqmhL._SX355_.jpg", remote_image_url: nil},
   {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2, image: "malm-high-bed-frame-storage-boxes__0173784_PE328440_S4.JPG", remote_image_url: nil},
   {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3, image: "41gBSNXd2VL.jpg", remote_image_url: nil},
   {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5, image: "dual_faucet_kegerators031813114530.jpg", remote_image_url: nil},
