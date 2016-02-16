8349e258b1e807b91441002f504479fad599988b
kaityhallman <kaitlinhallman@gmail.com>
2016-02-16 01:05:38 -0500

    ldfja

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-List.create!([
+List.create([
   {name: "Tires", category: "Automobile", price: 650, size: "Small", color: "Black", priority: 1},
   {name: "Dress", category: "Clothing", price: 65, size: "Small", color: "Blue", priority: 5},
   {name: "Kitchenaid Mixer", category: "Kitchen", price: 700, size: "n/s", color: "Red", priority: 3},
   {name: "Bed", category: "Furniture", price: 500, size: "medium", color: "Black", priority: 2},
-  {name: "French Press", category: "Kitchen", price: 40, size: "Medium", color: "N/A", priority: 3},
   {name: "Kegerator", category: "Kitchen", price: 600, size: "Medium", color: "Silver", priority: 5},
   {name: "Smart TV", category: "Electronics", price: 449, size: "Medium", color: "N/A", priority: 4}
 ])
